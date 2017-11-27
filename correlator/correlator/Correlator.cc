8993a7fa7c1897e8189786b7ef2e6b05a88a4cba
Alyssa Travitz <atravitz@Alyssas-MacBook-Pro.local>
2017-11-27 13:45:06 -0500

    updated .cc file to try to call logger

--- correlator/correlator/Correlator.cc
+++ correlator/correlator/Correlator.cc
@@ -10,10 +10,19 @@ Correlator::Correlator(std::shared_ptr<SystemDefinition> sysdef)
   }
 
 
-
 void Correlator::analyze(unsigned int timestep)
   {
-    if (m_prof) m_prof->push("Log"); //profiler
+    if (m_prof) m_prof->push("Correlator"); //profiler
 
+    // access the logged data values
+    assert(m_logger);
+    ArrayHandle<Scalar4> h_val(m_logger->getQuantity(), access_location::host, access_mode::read)
 
+  }
+
+void export_Correlator(pybind11:module& m)
+  {
+    pybind11::class_<Correlator, std::shared_ptr<Correlator> >(m, "Correlator", pybind11::base<Analyzer>())
+        .def(pybind11::init<std::shared_ptr<SystemDefinition> >())
+        ;
   }
