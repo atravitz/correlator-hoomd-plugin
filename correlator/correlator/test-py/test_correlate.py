cf467be0523ac13de6ed38afcbf05b662984bf9a
Alyssa Travitz <atravitz@Alyssas-MacBook-Pro.local>
2017-11-27 12:59:33 -0500

    updated test script

--- correlator/correlator/test-py/test_correlate.py
+++ correlator/correlator/test-py/test_correlate.py
@@ -1,6 +1,3 @@
-# -*- coding: iso-8859-1 -*-
-# Maintainer: joaander
-
 import hoomd;
 hoomd.context.initialize();
 import hoomd.correlate;
@@ -11,8 +8,11 @@ class test_simple(unittest.TestCase):
     def test_constructor(self):
         sysdef = hoomd.init.create_lattice(unitcell=hoomd.lattice.sq(a=2.0),
                                            n=[1,2]);
+        log = hoomd.analyze.log(filename=None, quantities='potential_energy', period=1)
 
-        analyzer = hoomd.correlate.analyze.example(4); #TODO if this pointing to a file somewhere?
+        correlate = hoomd.correlate.analyze(log)
+
+
 
 if __name__ == '__main__':
     unittest.main(argv = ['test_example.py', '-v'])
